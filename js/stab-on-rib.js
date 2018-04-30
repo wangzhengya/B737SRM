b67cc542b34bac417c960cbbc370ec62775185d7
zhengyuaing <wang_zhengya@163.com>
2018-04-30 19:15:59 +0800

    stab-on-rib

--- js/stab-on-rib.js
+++ js/stab-on-rib.js
@@ -11,22 +11,35 @@ $(function(){
     });
     
     $('#checkBtn').on('click', function(){
-        console.log($('#select-place').val(),$('#width').val(),$('#depth').val(),$('#select-width').val(),$('#select-depth').val());
-        console.log($('#no-pulled')[0].checked);
-        console.log($('#no-cracks')[0].checked);
         //$successDialog.fadeIn(200);
         var $selectPlace = $('#select-place').val();
         var $width = $('#width').val();
         var $depth = $('#depth').val();
         var $selectWidth = $('#select-width').val();
+        if($selectWidth == 1){
+            $width = $width / 25.4;
+        }
         var $selectDepth = $('#select-depth').val();
+        if($selectDepth == 1){
+            $depth = $depth / 25.4;
+        }
         var $noPulled = $('#no-pulled')[0].checked;
         var $noCracks = $('#no-cracks')[0].checked;
 
-        if($selectPlace == 1){
-            
-        }else if($selectPlace == 2){
-
+        if($selectPlace == 1){//翼肋和梁搭接处<
+            console.log("翼肋和梁搭接处\n宽度："+$width+"深度："+$depth);
+            if($depth < 0.02 && ($width/$depth)>=30 && $noPulled && $noCracks){
+                $successDialog.fadeIn(200);
+            }else{
+                $failDialog.fadeIn(200);
+            }
+        }else if($selectPlace == 2){//翼肋和蒙皮搭接处
+            console.log("翼肋和蒙皮搭接处\n宽度："+$width+"深度："+$depth);
+            if($depth < 0.03 && ($width/$depth)>=30 && $noPulled && $noCracks){
+                $successDialog.fadeIn(200);
+            }else{
+                $failDialog.fadeIn(200);
+            }
         }else{
             $inputDialog.fadeIn(200);
         }
