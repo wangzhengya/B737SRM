bbb4cec8599bd86e891a2cba60d0f8c17ddc8194
zhengyuaing <wang_zhengya@163.com>
2018-11-30 19:58:36 +0800

    dayi

---
+++ js/flap.js
@@ -0,0 +1,39 @@
+//凹坑在翼肋上的判断条件
+$(function(){
+
+    var $successDialog = $('#success-dialog');
+    var $failDialog = $('#fail-dialog');
+    var $inputDialog = $('#input-dialog');
+    
+
+    $('#dialogs').on('click', '.weui-dialog__btn', function(){
+        $(this).parents('.js_dialog').fadeOut(200);
+    });
+    
+    $('#checkBtn').on('click', function(){
+        //$successDialog.fadeIn(200);
+        var $selectPlace = $('#select-place').val();
+        var $width = $('#width').val();
+        var $depth = $('#depth').val();
+        var $selectWidth = $('#select-width').val();
+        if($selectWidth == 1){
+            $width = $width / 25.4;
+        }
+        var $selectDepth = $('#select-depth').val();
+        if($selectDepth == 1){
+            $depth = $depth / 25.4;
+        }
+        var $noPulled = $('#no-pulled')[0].checked;
+        var $noCracks = $('#no-cracks')[0].checked;
+
+
+            console.log("翼肋和蒙皮搭接处\n宽度："+$width+"深度："+$depth);
+            if($depth <= 0.125 && ($width/$depth)>=30 && $noPulled && $noCracks){
+                $successDialog.fadeIn(200);
+            }else{
+                $failDialog.fadeIn(200);
+            }
+
+    });
+    
+});
